334e6bde3653d676be342289c9114f9bdbbc30eb
Finn Hediger <finn.hediger@thurweb.ch>
2021-09-08 19:42:12 +0200

    Change naming from var_name to varName

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,18 @@
 #include <Arduino.h>
 
 #include "joystick.h"
-#include "move.h"
+#include "stepper.h"
 
 // The whiteboard (in mm)
-int board_width = 1930;
-int board_height = 1170;
+int boardWidth = 1930;
+int boardHeight = 1170;
 // The current position (s1, s2)
 // s1 = left string, s2 = right string
 int position[2] = {1516, 1516};
 
-float base_velocity = 1.0;
+float baseVelocity = 1.0;
 // TODO: measure/calculate exact mm per step
 float perstep = 0.019625; // 0.07925;
-
-struct travel_args {
-  float velocity;
-  int distance;
-  int stepper;
-};
-// Make the string for a certain motor longer/shorter
-void *travel(void *args) {
-  int stepper = ((struct travel_args *)args)->stepper;
-  int distance = ((struct travel_args *)args)->distance;
-  float velocity = ((struct travel_args *)args)->velocity;
-
-  // Set the direction
-  digitalWrite(dirPins[stepper], distance > 0 ? stepper : (int)!(bool)stepper);
-  // Calculate and do steps
-  int steps = abs(distance)/perstep;
-  TickType_t x_last_wake_time = xTaskGetTickCount();
-  for (int i = 0; i < steps; i++) {
-    step(stepper);
-    // delayMicroseconds will not work,
-    // because it does not provide time for the watchdog
-    // vTaskDelay is absolute, vTaskDelayUntil is relative
-    vTaskDelayUntil(&x_last_wake_time, velocity * portTICK_PERIOD_MS);
-    // vTaskDelay(velocity * portTICK_PERIOD_MS);
-  }
-
-  return 0;
-}
 
 // int old_go_to(int x, int y) {
 //   // Calculate the new length of the string
@@ -64,51 +36,50 @@ void *travel(void *args) {
 //   return 0;
 // }
 
-int go_to(int x, int y) {
+int goTo(int x, int y) {
   Serial.printf("Going to (%d, %d) ...", x, y);
 
-  // Calculate the new length of the string
-  int new_s1 = sqrt(pow(board_width/2 + x, 2) + pow(board_height - y, 2));
-  int new_s2 = sqrt(pow(board_width/2 - x, 2) + pow(board_height - y, 2));
+  // Calculate the new length of the string with the
+  // Pythagoras formulae
+  int newS1 = sqrt(pow(boardWidth/2 + x, 2) + pow(boardHeight - y, 2));
+  int newS2 = sqrt(pow(boardWidth/2 - x, 2) + pow(boardHeight - y, 2));
   // Calculate the necessary movement
-  int distance_s1 = new_s1 - position[0];
-  int distance_s2 = new_s2 - position[1];
+  int distanceS1 = newS1 - position[0];
+  int distanceS2 = newS2 - position[1];
   // Update the position
-  position[0] = new_s1;
-  position[1] = new_s2;
+  position[0] = newS1;
+  position[1] = newS2;
 
-  Serial.printf("Distances: %d, %d\n", distance_s1, distance_s2);
+  Serial.printf("Distances: %d, %d\n", distanceS1, distanceS2);
   Serial.printf("New position:  %d, %d\n", position[0], position[1]);
 
   // Calculate the needed velocities
-  float velocity_s1, velocity_s2;
-  if (distance_s1 < distance_s2) {
-    velocity_s1 = base_velocity;
-    velocity_s2 = (float)abs(distance_s1) / (float)abs(distance_s2) * velocity_s1;
+  float velocityS1, velocityS2;
+  if (distanceS1 < distanceS2) {
+    velocityS1 = baseVelocity;
+    velocityS2 = (float)abs(distanceS1) / (float)abs(distanceS2) * velocityS1;
   } else {
-    velocity_s2 = base_velocity;
-    velocity_s1 = (float)abs(distance_s2) / (float)abs(distance_s1) * velocity_s2;
+    velocityS2 = baseVelocity;
+    velocityS1 = (float)abs(distanceS2) / (float)abs(distanceS1) * velocityS2;
   }
 
-  Serial.printf("Velocities: %f, %f\n", velocity_s1, velocity_s2);
+  Serial.printf("Velocities: %f, %f\n", velocityS1, velocityS2);
 
   // Spawn two threads, one for each motor
   pthread_t threads[2];
-  struct travel_args to_travel_s1 = {velocity_s1, distance_s1, 0};
-  struct travel_args to_travel_s2 = {velocity_s2, distance_s2, 1};
-  int return_val_s1 = pthread_create(&threads[0], NULL, travel, &to_travel_s1);
-  int return_val_s2 = pthread_create(&threads[1], NULL, travel, &to_travel_s2);
-  if (return_val_s1 != 0 || return_val_s2 != 0) {
+  int creationS1 = pthread_create(&threads[0], NULL, travel, &distanceS1);
+  int creationS2 = pthread_create(&threads[1], NULL, travel, &distanceS2);
+  if (creationS1 != 0 || creationS2 != 0) {
     Serial.println("Failed to create threads!");
     return 1;
   }
 
   // Wait for the threads to complete
   // and check the status code
-  void *status_s1, *status_s2;
-  pthread_join(threads[0], &status_s1);
-  pthread_join(threads[1], &status_s2);
-  if (status_s1 != 0 || status_s2 != 0) {
+  void *statusS1, *statusS2;
+  pthread_join(threads[0], &statusS1);
+  pthread_join(threads[1], &statusS2);
+  if (statusS1 != 0 || statusS2 != 0) {
     Serial.println("Failed to move!");
     return 1;
   }
@@ -125,11 +96,12 @@ void setup() {
   for (int p = 0; p < 2; p++)
     pinMode(stepPins[p], OUTPUT);
   // Move to a coordinate
-  if (go_to(200, 100) != 0) {
+  if (goTo(200, 100) != 0) {
     Serial.println("Could not move to coordinate!");
   }
 }
 
 void loop() {
+  // Fall back to joystick control
   joystick();
 }
