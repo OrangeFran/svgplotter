7d9f7918da4b6721cafa037ebc57cb0f79c6ceda
Finn Hediger <finn.hediger@thurweb.ch>
2021-09-19 11:37:45 +0200

    Add parseSVG
    The parser goes over each character and extracts attribute values specified in the attributes[] array.
    
    Current values to extract: { 'svg.viewBox', 'path.d' }.

--- lib/parser/parser.cpp
+++ lib/parser/parser.cpp
@@ -2,83 +2,164 @@
 // Only the <path> and <svg> elements are needed
 
 #include <string>
-#include <sstream>
 #include <iostream>
 
 const std::string testString =
   "<?xml version=\"1.0\" ?>\n"
   "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
-  "<path d=\"M 100 100\" blabla=\"error\">\n"
+  "<path color=\"blue\" d=\"M 100 100\" blabla=\"error\">\n"
   "</svg>";
 
-struct svg {
-  int viewBox[4];
-  // The path will be parsed and executed simultaneously
+struct SVG {
+  std::string viewBox;
+  // The paths will be parsed and executed simultaneously
   std::string path;
 };
 
-// Extracts the value of attribute ATTR from a tag
-// Example: "viewBox" from <svg viewBox="0 0 100 100">
-//          -> "0 0 100 100"
-std::string extractValue(std::string attr, std::string tag) {
-  std::string res;
-  // Loop until the attribute is found
-  while (true) {
-    // .substr includes the starting index -> add 1
-    tag = tag.substr(tag.find(' ') + 1, -1);
-    std::cout << "Checking '" << tag << "'" << std::endl;
-    // Check if the substr starts with the attribute
-    if (tag.rfind(attr, 0) != -1) {
-      int start = tag.find('"');
-      // Is there an easier way?
-      res = tag.substr(start + 1, tag.substr(start + 1, -1).find('"'));
-      break;
+// // Extracts the value of attribute ATTR from a tag
+// // Example: "viewBox" from <svg viewBox="0 0 100 100">
+// //          -> "0 0 100 100"
+// std::string extractValue(std::string attr, std::string tag) {
+//   std::string res;
+//   // Loop until the attribute is found
+//   while (true) {
+//     // .substr includes the starting index -> add 1
+//     tag = tag.substr(tag.find(' ') + 1, -1);
+//     // Check if the substr starts with the attribute
+//     if (tag.rfind(attr, 0) != -1) {
+//       int start = tag.find('"');
+//       // Is there an easier way?
+//       res = tag.substr(start + 1, tag.substr(start + 1, -1).find('"'));
+//       break;
+//     }
+//   }
+//   return res;
+// }
+
+enum ParserState {
+  PARSER_findTag = 1,   // Looks for tag start ('<')
+  PARSER_getTag = 2,    // Checks if tag is either 'path' or 'svg'
+  PARSER_findAttr = 3,  // Finds a certain attribute ('viewBox' or 'd')
+  PARSER_getAttr = 4,   // Extracts value of attribute
+  PARSER_endTag = 5,    // Finds end of tag ('>')
+};
+
+// Attributes to look for and extract
+const std::string attributes[2] = { "svg.viewBox", "path.d" };
+
+// Parse a valid svg into a struct
+// Parser assumes svg is valid and does
+// not check for validity
+struct SVG parseSVG(std::string str) {
+  // Resulting svg
+  struct SVG svg;
+  
+  std::string buff;                       // Temporary storage
+  std::string attr;                         // Attribute to search for ('viewBox' or 'd' for now)
+  enum ParserState state = PARSER_findTag;  // The current state
+  bool escaped = false;                     // True if previous character was '\'
+
+  // Loops through every char of str
+  for (char c: str) {
+    if (escaped) {
+      // Currently extracting value and previous char was '\' 
+      // Treat is as a normal character 
+      if (state == PARSER_getAttr) {
+        buff += c;
+        continue;
+      }
+      escaped = false;
+    }
+    switch (state) {
+      case PARSER_findTag:
+        if (c == '<') {
+          buff = "";
+          state = PARSER_getTag;
+        }
+        break;
+      case PARSER_getTag:
+        if (c == ' ') {
+          // To implement more, just add if statements
+          for (int i = 0; i < 2; i++) {
+            size_t p = attributes[i].find('.');
+            if (buff == attributes[i].substr(0, p)) {
+              state = PARSER_findAttr;
+              attr = attributes[i].substr(p + 1, -1);
+              buff = "";
+              break;
+            }
+          }
+          // If we don't need the tag, just look for the end
+          if (attr == "") {
+            state = PARSER_endTag;
+          }
+        } else {
+          buff += c;
+        }
+        break;
+      case PARSER_endTag:
+        if (c == '>') {
+          state = PARSER_findTag;
+          buff = "";
+        }
+        break;
+      case PARSER_findAttr:
+        if (c == ' ') {
+          buff = "";
+        // Attribute name ends
+        } else if (c == '=') {
+          if (buff == attr) {
+            state = PARSER_getAttr;
+            buff = "";
+          }
+        } else {
+          buff += c;
+        }
+        break;
+      case PARSER_getAttr:
+        if (c == '"') {
+          // End of attribute value
+          // TODO: Handle edge case that attribute value is empty -> infinite loop
+          if (buff != "") {
+            if (attr == "viewBox") {
+              svg.viewBox = buff;
+              state = PARSER_findTag;
+              attr = "";
+            } else if (attr == "d") {
+              svg.path = buff;
+              state = PARSER_findTag;
+              attr = "";
+            }
+            buff = "";
+          }
+        } else if (c == '\\') {
+          escaped = true;
+        } else {
+          buff += c;
+        }
+        break;
     }
   }
-  return res;
+
+  return svg;
 }
 
 int main() {
-  std::string tag = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n";
-  std::string res = extractValue("viewBox", tag);
-  std::cout << "'" << res << "'" << std::endl;
+  // // Test 1
+  // std::string tag = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n";
+  // std::string res = extractValue("viewBox", tag);
+  // // "0 0 500 500" is expected
+  // std::cout << "'" << res << "'" << std::endl;
 
-  std::string tag2 = "<path d=\"M 100 100\" blabla=\"error\">\n";
-  std::string res2 = extractValue("d", tag2);
-  std::cout << "'" << res2 << "'" << std::endl;
+  // // Test 2
+  // std::string tag2 = "<path d=\"M 100 100\" blabla=\"error\">\n";
+  // std::string res2 = extractValue("d", tag2);
+  // // "M 100 100" is expected
+  // std::cout << "'" << res2 << "'" << std::endl;
+
+  // Test 1
+  struct SVG svg = parseSVG(testString);
+  std::cout << "svg.viewBox: '" << svg.viewBox << "'" << std::endl;
+  std::cout << "path.d: '" << svg.path << "'" << std::endl;
+  return 0;
 }
-
-// // Get the root svg tag and the viewBox
-// struct svg *parseSVG(std::string str) {
-//   // str.find(); str.substr()
-//   struct svg resultSVG = {};
-//   std::string splitted[] = strtok(str, " ");
-// 
-//   bool inSVG, inPath = false;
-//   for (int i = 0; i > 100; i++) {
-//     // Change the context if necessary
-//     switch (splitted[i]) {
-//       case "<svg":
-//         inSVG = true;
-//       case "<path":
-//         inPath = true;
-//       case ">":
-//         inSVG = false;
-//         inPath = false;
-//       default:
-//     }
-//     // Do an action 
-//     if (inSVG) {
-//       // Find the viewBox
-//       std::size_t pos = splitted[i].rfind("viewBox", 0);
-//       if (pos == 0) {
-//         std::string viewBox = splitted[i].substr(splitted[i].find("\""));
-//         // Loop further to find the end of the string (")
-//         while (splitted[i].at(splitted[i].size()) != "\"") {
-//           viewBox += splitted[i];
-//           i += 1;
-//         }
-//       }
-//     }
-//   }
-// }
