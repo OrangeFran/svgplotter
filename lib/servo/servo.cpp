349e3e073512eed02cb761664ccdc04ae64a9a02
Finn Hediger <finn.hediger@thurweb.ch>
2021-10-19 14:24:39 +0200

    Add new servo library to control servo for the pen

---
+++ lib/servo/servo.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "servo.h"
+
+const int servoPin = 22;
+
+Servo::Servo(int pin) {
+  this->pin = pin;
+  this->down = false;
+  
+  // Setup pwm signal
+  pinMode(this->pin, OUTPUT);
+  ledcSetup(8, 50, 8);
+  ledcAttachPin(this->pin, 8);
+}
+
+int Servo::penDown() {
+  if (!down) {
+    this->down = true;
+    ledcWrite(8, (int)(1.0/20.0 * 256.0));
+    delay(500);
+    ledcWrite(8, 0);
+  }
+  return 0;
+}
+
+int Servo::penUp() {
+  if (down) {
+    this->down = false;
+    ledcWrite(8, (int)(1.5/20.0 * 256.0));
+    delay(500);
+    ledcWrite(8, 0);
+  }
+  return 0;
+}
+
+int Servo::toggle() {
+  if (down) {
+    this->penUp();
+  } else {
+    this->penDown();
+  }
+  return 0;
+}
