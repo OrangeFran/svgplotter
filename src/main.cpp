b52da56eeb0c3cc47f38599426391dae813cf0dc
Finn Hediger <finn.hediger@thurweb.ch>
2021-10-19 14:26:22 +0200

    Remove drawing from main function, only keep joystick

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,28 @@ StepperMotor stepper2 = StepperMotor(1, dirPins[1], stepPins[1]);
 Point start = Point(0, 0);
 float *startStringLength = start.calculatePosition();
 
+Servo pen = Servo(penPin);
+
 Plotter plotter = {
   .pos = start, 
   // NOTE: Better way?
   .stringLength = { startStringLength[0], startStringLength[1] },
   .stepper1 = stepper1,
   .stepper2 = stepper2,
+  .pen =  pen,
 };
 
 void setup() {
   Serial.begin(9600);
   setMotorState(true);
-  delay(5000);
-
-  // Parse example SVG
-  const std::string example =
-    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
-      "<path d=\"M 100 200 L 200 300\">\n"
-    "</svg>";
-  SVG svg = SVG(example);
-  plotter.executeSVG(svg);
+  delay(2000);
 
   // Disconnect the pins from the PWM signal
   ledcDetachPin(plotter.stepper1.stepPin);
   ledcDetachPin(plotter.stepper2.stepPin);
+
+  // Fall back to joystick control
+  joystick(plotter);
 }
 
-void loop() {
-  // Fall back to joystick control
-  joystick();
-}
+void loop() {}
