62b79d6e97d5c4d6e48e24ebdac6dcac7d871fbd
Finn Hediger <finn.hediger@thurweb.ch>
2021-11-06 22:12:49 +0100

    Add first try at variable accuracy

--- lib/plotter/bezier.cpp
+++ lib/plotter/bezier.cpp
@@ -25,9 +25,12 @@ int Plotter::bezierLinear(Point p1) {
 
 // `B(t) = (1 - t)^2 P_0 + 2t (1 - t) P_1 + t^2 P_2`
 int Plotter::bezierQuadratic(Point p1, Point p2) {
-  // `accuracy` defines the amount of steps between
   float x, y;
-  float accuracy = 20.0;
+  // `accuracy` defines the amount of steps between
+  // Add up the distances between all points / ... -> accuracy (capped at 50)
+  float aproxLength =
+    pow(pow(this->pos.x - p1.x, 2) + pow(this->pos.y - this->pos.y, 2), 0.5) + pow(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2), 0.5);
+  float accuracy = aproxLength < 50 ? aproxLength : 50;
   float increase = 1.0/accuracy;
   // Move parameter t from 0.0 to 1.0
   for (float t = increase; t <= (float)1.0; t += increase) {
@@ -41,9 +44,13 @@ int Plotter::bezierQuadratic(Point p1, Point p2) {
 
 // `B(t) = (1 - t)^3 P_0 + 3t (1 - t)^2 P_1 + 3t^2 (1 - t) P_2 + t^3 P_3`
 int Plotter::bezierCubic(Point p1, Point p2, Point p3) {
-  // `accuracy` defines the amount of steps between
   float x, y;
-  float accuracy = 50.0;
+  // `accuracy` defines the amount of steps between
+  // Add up the distances between all points / ... -> accuracy (capped at 50)
+  float aproxLength =
+    pow(pow(this->pos.x - p1.x, 2) + pow(this->pos.y - this->pos.y, 2), 0.5)
+      + pow(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2), 0.5) + pow(pow(p2.x - p3.x, 2) + pow(p2.y - p3.y, 2), 0.5);
+  float accuracy = aproxLength < 50 ? aproxLength : 50;
   float increase = 1.0/accuracy;
   for (float t = increase; t <= (float)1.0; t += increase) {
     x = pow((1.0 - t), 3) * this->pos.x + 3.0 * t * pow((1.0 - t), 2) * p1.x + 3.0 * pow(t, 2) * (1.0 - t) * p2.x + pow(t, 3) * p3.x;
