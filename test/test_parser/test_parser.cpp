010a8e26ae2a3603cd9938970ee0e1cd8459e1e3
Finn Hediger <finn.hediger@thurweb.ch>
2021-11-25 11:49:04 +0100

    Fix tests

--- test/test_parser/test_parser.cpp
+++ test/test_parser/test_parser.cpp
@@ -5,32 +5,6 @@
 
 #include "parser.h"
 
-void test_parserr_valid_find_path_default() {
-  const std::string s =
-    // "<?xml version=\"1.0\" ?>\n"
-    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
-      "<path color=\"blue\" d=\"M 100 100\">\n"
-    "</svg>";
-
-  CustomStream *sstream = new StringStream(s);
-
-  TEST_ASSERT_EQUAL_STRING("M 100 100", findPath(sstream).c_str());
-}
-
-void test_parser_valid_svg_multiple_path() {
-  const std::string s =
-    // "<?xml version=\"1.0\" ?>\n"
-    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
-      "<path color=\"blue\" d=\"M 100 100\">\n"
-      "<path color=\"red\" d=\"L 200 200\">\n"
-    "</svg>";
-
-  CustomStream *sstream = new StringStream(s);
-
-  TEST_ASSERT_EQUAL_STRING("M 100 100", findPath(sstream).c_str());
-  TEST_ASSERT_EQUAL_STRING("L 200 200", findPath(sstream).c_str());
-}
-
 void test_parser_valid_svg_viewbox_float() {
   const std::string s =
     // "<?xml version=\"1.0\" ?>\n"
@@ -45,7 +19,33 @@ void test_parser_valid_svg_viewbox_float() {
   TEST_ASSERT_EQUAL_FLOAT_ARRAY(expectedViewBox, svg.viewBox, 4);
 }
 
-void test_parser_valid_svg_default() {
+void test_parser_valid_path_str() {
+  const std::string s =
+    // "<?xml version=\"1.0\" ?>\n"
+    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
+      "<path color=\"blue\" d=\"M 100 100\">\n"
+    "</svg>";
+
+  CustomStream *sstream = new StringStream(s);
+
+  TEST_ASSERT_EQUAL_STRING("M 100 100", findPath(sstream).c_str());
+}
+
+void test_parser_valid_multiple_path_str() {
+  const std::string s =
+    // "<?xml version=\"1.0\" ?>\n"
+    "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
+      "<path color=\"blue\" d=\"M 100 100\">\n"
+      "<path color=\"red\" d=\"L 200 200\">\n"
+    "</svg>";
+
+  CustomStream *sstream = new StringStream(s);
+
+  TEST_ASSERT_EQUAL_STRING("M 100 100", findPath(sstream).c_str());
+  TEST_ASSERT_EQUAL_STRING("L 200 200", findPath(sstream).c_str());
+}
+
+void test_parser_valid_svg() {
   const std::string s =
     // "<?xml version=\"1.0\" ?>\n"
     "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
@@ -65,7 +65,7 @@ void test_parser_valid_svg_default() {
 }
 
 
-void test_parser_valid_path_default() {
+void test_parser_valid_parse_path() {
   const std::string s =
     // "<?xml version=\"1.0\" ?>\n"
     "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
@@ -80,7 +80,7 @@ void test_parser_valid_path_default() {
   TEST_ASSERT_EQUAL_FLOAT(100.0, path[0].second[1]);
 }
 
-void test_parser_valid_path_float() {
+void test_parser_valid_parse_path_float() {
   const std::string s =
     // "<?xml version=\"1.0\" ?>\n"
     "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
@@ -95,7 +95,7 @@ void test_parser_valid_path_float() {
   TEST_ASSERT_EQUAL_FLOAT(100.145, path[0].second[1]);
 }
 
-void test_parser_valid_path_no_space() {
+void test_parser_valid_parse_path_no_space() {
   const std::string s =
     // "<?xml version=\"1.0\" ?>\n"
     "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 500 500\">\n"
@@ -114,11 +114,13 @@ void test_parser_valid_path_no_space() {
 // `pio test -e native -v`
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(test_parserr_valid_find_path_default);
-  RUN_TEST(test_parser_valid_svg_default);
-  RUN_TEST(test_parser_valid_svg_multiple_path);
   RUN_TEST(test_parser_valid_svg_viewbox_float);
-  RUN_TEST(test_parser_valid_path_default);
+  RUN_TEST(test_parser_valid_path_str);
+  RUN_TEST(test_parser_valid_multiple_path_str);
+  RUN_TEST(test_parser_valid_svg);
+  RUN_TEST(test_parser_valid_parse_path);
+  RUN_TEST(test_parser_valid_parse_path_float);
+  RUN_TEST(test_parser_valid_parse_path_no_space);
   UNITY_END();
 }
 
