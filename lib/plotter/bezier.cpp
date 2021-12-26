c3951707f33eae42607d7394f435925fe91d34a5
Finn Hediger <finn.hediger@thurweb.ch>
2021-12-26 11:56:57 +0100

    Implement new approximation algorithm (WIP)

--- lib/plotter/bezier.cpp
+++ lib/plotter/bezier.cpp
@@ -52,3 +52,92 @@ void Plotter::bezierCubic(Point p1, Point p2, Point p3) {
 
   delay(100);
 }
+
+// Test this later
+Point *pointAtBezierQuadratic(float t) {
+  float x = pow((1.0 - t), 2) * p0.x + 2.0 * t * (1.0 - t) * p1.x + pow(t, 2) * p2.x;
+  float y = pow((1.0 - t), 2) * p0.y + 2.0 * t * (1.0 - t) * p1.y + pow(t, 2) * p2.y;
+  return new Point(x, y);
+}
+
+void bezierQuadraticNew(Point p1, Point p2) {
+  const float thresh = 0.1;
+
+  float t = 1;
+  float T = 0;
+  float Tx, Ty;
+  float mT, dTx, dTy;
+  while (true) {
+    // TODO: Check the derivative
+    // Calculate velocity of bezier curve 
+    dTx = 2 * p2.x * t - 2 * p1.x * t + 2 * p1.x * (1 - t) - 2 * p0.x (1 - t)
+    dTy = 2 * p2.y * t - 2 * p1.y * t + 2 * p1.y * (1 - t) - 2 * p0.y (1 - t)
+    mT = dTy / dTx;
+    // Calculate the current point
+    Tx = pow((1.0 - t), 2) * p0.x + 2.0 * t * (1.0 - t) * p1.x + pow(t, 2) * p2.x;
+    Ty = pow((1.0 - t), 2) * p0.y + 2.0 * t * (1.0 - t) * p1.y + pow(t, 2) * p2.y;
+    // Try to approximate with threshold
+    float tx, ty, m;
+    while (true) {
+      tx = pow((1.0 - t), 2) * p0.x + 2.0 * t * (1.0 - t) * p1.x + pow(t, 2) * p2.x;
+      ty = pow((1.0 - t), 2) * p0.y + 2.0 * t * (1.0 - t) * p1.y + pow(t, 2) * p2.y;
+      m = (ty - Ty) / (tx - Tx);
+      if ((mT - m) < thresh) {
+        T = t;
+        t = 0;
+        break;
+      }
+      // Split t and try again
+      t = (t - T) / 2
+    }
+
+    // Draw the newly calculated segment
+    this->moveTo(Point(tx, ty));
+
+    // Curve is finished
+    if (T == 1) {
+      break;
+    }
+  }
+}
+
+void bezierCubicNew(Point p1, Point p2, Point p3) {
+  const float thresh = 0.1;
+
+  float t = 1;
+  float T = 0;
+  float Tx, Ty;
+  float mT, dTx, dTy;
+  while (true) {
+    // TODO: Check the derivative
+    // Calculate velocity of bezier curve 
+    dTx = 3 * p3.x * pow(t, 2) - 3 * p2.x * pow(t, 2) + 6 * p2.x * (1 - t) * t - 6 * p1.x * (1 - t) * t + 3 * p1.x * pow((1 - t), 2) - 3 * p0.x * pow((1 - t), 2);
+    dTy = 3 * p3.y * pow(t, 2) - 3 * p2.y * pow(t, 2) + 6 * p2.y * (1 - t) * t - 6 * p1.y * (1 - t) * t + 3 * p1.y * pow((1 - t), 2) - 3 * p0.y * pow((1 - t), 2);
+    mT = dTy / dTx;
+    // Calculate the current point
+    Tx = pow((1.0 - t), 3) * p0.x + 3.0 * t * pow((1.0 - t), 2) * p1.x + 3.0 * pow(t, 2) * (1.0 - t) * p2.x + pow(t, 3) * p3.x;
+    Ty = pow((1.0 - t), 3) * p0.y + 3.0 * t * pow((1.0 - t), 2) * p1.y + 3.0 * pow(t, 2) * (1.0 - t) * p2.y + pow(t, 3) * p3.y;
+    // Try to approximate with threshold
+    float tx, ty, m;
+    while (true) {
+      tx = pow((1.0 - t), 3) * p0.x + 3.0 * t * pow((1.0 - t), 2) * p1.x + 3.0 * pow(t, 2) * (1.0 - t) * p2.x + pow(t, 3) * p3.x;
+      ty = pow((1.0 - t), 3) * p0.y + 3.0 * t * pow((1.0 - t), 2) * p1.y + 3.0 * pow(t, 2) * (1.0 - t) * p2.y + pow(t, 3) * p3.y;
+      m = (ty - Ty) / (tx - Tx);
+      if ((mT - m) < thresh) {
+        T = t;
+        t = 0;
+        break;
+      }
+      // Split t and try again
+      t = (t - T) / 2
+    }
+
+    // Draw the newly calculated segment
+    this->moveTo(Point(tx, ty));
+
+    // Curve is finished
+    if (T == 1) {
+      break;
+    }
+  }
+}
