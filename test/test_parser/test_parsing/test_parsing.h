82d6e2ce49223b21677e30afe5dbb5f937676eb7
Finn Hediger <finn.hediger@thurweb.ch>
2021-12-29 13:46:04 +0100

    Split up tests even more

---
+++ test/test_parser/test_parsing/test_parsing.h
@@ -0,0 +1,12 @@
+#ifndef TEST_PARSING_H
+#define TEST_PARSING_H
+
+void test_parser_valid_svg_viewbox_float();
+void test_parser_valid_path_str();
+void test_parser_valid_multiple_path_str();
+void test_parser_valid_svg();
+void test_parser_valid_parse_path();
+void test_parser_valid_parse_path_float();
+void test_parser_valid_parse_path_no_space();
+
+#endif
